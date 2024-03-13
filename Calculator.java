9486c8030f448dc0062c97a71476cc1ca6bdb8b0
Timo Kubera <t.kubera@reply.de>
2024-03-13 14:59:26 +0100

    Inefficient code; lulz

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,10 @@ import java.util.List;
 public class Calculator {
     public int calculateSum(List<Integer> numbers) {
         int sum = 0;
-        for (int number : numbers) {
-            sum += number;
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 0; j < numbers.get(i); j++) {
+                sum++;
+            }
         }
         return sum;
     }
