02d56fe2c43bb4639a95f8357f8defc69a1b1510
Timo Kubera <t.kubera@reply.de>
2024-03-13 14:58:43 +0100

    Improving calc

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,6 @@ import java.util.List;
 
 public class Calculator {
     public int calculateSum(List<Integer> numbers) {
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-        return sum;
+        return numbers.stream().mapToInt(Integer::intValue).sum();
     }
 }
