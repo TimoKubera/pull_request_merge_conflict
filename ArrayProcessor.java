0e0378de3708ac1724ff4077bb0a4643601c4048
Timo Kubera <t.kubera@reply.de>
2024-03-13 15:07:43 +0100

    more changes

---
+++ ArrayProcessor.java
@@ -0,0 +1,21 @@
+public class ArrayProcessor {
+    public int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
---
+++ PBCalculator.java
@@ -0,0 +1,21 @@
+public class PBCalculator {
+    public int add(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public int subtract(int a, int b) {
+        int result = a - b;
+        return result;
+    }
+
+    public int multiply(int a, int b) {
+        int result = a * b;
+        return result;
+    }
+
+    public int divide(int a, int b) {
+        int result = a / b;
+        return result;
+    }
+}
